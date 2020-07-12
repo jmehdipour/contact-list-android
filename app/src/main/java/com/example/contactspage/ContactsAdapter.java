0419a347926d9f6d95ea99114e7aabaac9e657f4
ilya mehdipour <mehdipour.py@gmail.com>
2020-07-12 05:06:09 -0700

    add onclick listener

--- app/src/main/java/com/example/contactspage/ContactsAdapter.java
+++ app/src/main/java/com/example/contactspage/ContactsAdapter.java
@@ -1,29 +1,56 @@
 package com.example.contactspage;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ContactViewHolder> {
+    private String[] contacts = new String[20];
 
+    public ContactsAdapter(){
+        contacts[0]="Ruthann Trustrie";
+        contacts[1]="Peadar Dawtrey";
+        contacts[2]="Felipe Bradtke";
+        contacts[3]="Claude Crissil";
+        contacts[4]="Jacky Girardeau";
+        contacts[5]="Rubia Dominguez";
+        contacts[6]="Michaela Churchley";
+        contacts[7]="Harvey Pentelow";
+        contacts[8]="Neilla Langton";
+        contacts[9]="Marco Greaves";
+        contacts[10]="Liz Batchley";
+        contacts[11]="Lamond Littlepage";
+        contacts[12]="Malina Weir";
+        contacts[13]="Tomlin Lenchenko";
+        contacts[14]="Hy Pavelin";
+        contacts[15]="Jenelle Palin";
+        contacts[16]="Damon Knewstubb";
+        contacts[17]="Alex Ivanusyev";
+        contacts[18]="Hamil Callery";
+        contacts[19]="Karol Syer";
+
+    }
 
     @NonNull
     @Override
     public ContactViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_contact, parent, false);
+        return new ContactViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ContactViewHolder holder, int position) {
-
+        holder.bindContact(contacts[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return contacts.length;
     }
 
     public class ContactViewHolder extends RecyclerView.ViewHolder{
@@ -36,9 +63,15 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.Contac
             fullNameTv = itemView.findViewById(R.id.tv_contact_fullName);
         }
 
-        public void bindContact(String fullName){
+        public void bindContact(final String fullName){
             firstCharacterTv.setText(fullName.substring(0,1));
             fullNameTv.setText(fullName);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(view.getContext(), fullName, Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
